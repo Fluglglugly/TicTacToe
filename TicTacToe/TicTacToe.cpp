a9f0c6679063a5bebe90a62cabd229bd18d65ad1
Fluglglugly <70482373+Fluglglugly@users.noreply.github.com>
2020-09-28 21:34:29 +0200

    update

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -2,15 +2,16 @@
 
 #include <iostream>
 #include <string>
-#include <list>
-#include <iterator>
 
+//Defines what string-value should be used fro the different players, and the string values that I change when making a move
 std::string Player[2] = { " X "," O " };
 std::string A[3] = { "   ","   ","   " };
 std::string B[3] = { "   ","   ","   " };
 std::string C[3] = { "   ","   ","   " };
-
+//Counter is just a counter that alternates between 1 and 0, exept for when the game is over, when it goes above 1, thereby breaking the loop
 int Counter = 0;
+int TurnTimer = 0;
+
 
 void func_Board() {
         std::cout << "   A   B   C\n";
@@ -22,7 +23,7 @@ void func_Board() {
 void func_Turn() {
     char Letter = 'E';
     int Number = 0;
-    std::cout << "Player"<<Player[Counter]<<"make a move\n";
+    std::cout << "Player"<<Player[Counter]<<"make a move\n"; //this is where Counter decides which players turn it is
     std::cin >> Letter >> Number;
     if (Letter == 'A') {
         A[Number - 1] = Player[Counter];
@@ -33,6 +34,7 @@ void func_Turn() {
     else if (Letter == 'C') {
         C[Number - 1] = Player[Counter];
     }
+    TurnTimer += 1; //Is used to decide if all tiles should be filled in and the game should end. I know there are better ways, but I'm lazy and it works
     func_Board();
 
 }
@@ -43,6 +45,7 @@ void func_Win() {
     Counter += 4;
 
 }
+
 
 //Wanted to loop through the variables as well as the numbers, but wasn't sure how to do it without making the variable names into a string, meaning they won't call the value I've assigned them
 //Is there a good way of looping thorugh different defined variables?
@@ -55,6 +58,8 @@ void func_WinCondition() {
         }
         D += 1;
     }
+    //I'm supposed to write comments here, but not sure what to write other than the fact that I'm defining what the winconditions are and what to do if they are met...
+    //Includes the statement above this comment
     if (A[0] != "   " & A[0] == A[1] & A[1] == A[2]) {
         func_Win();
 
@@ -71,7 +76,11 @@ void func_WinCondition() {
     else if (A[2] != "   " & A[2] == B[1] & B[1] == C[0]) {
         func_Win();
     }
-
+    else if (TurnTimer == 9) {
+        std::cout << "It's a draw!";
+        Counter += 4;
+    }
+    
 }
 
 
@@ -79,17 +88,17 @@ void func_WinCondition() {
 int main()
 {
     func_Board();
-    while (Counter < 3) 
+    while (Counter < 2) //runs the game
     {
-        func_Turn();
-        func_WinCondition();
-        if (Counter == 0){
+        func_Turn(); //runs a single turn
+        func_WinCondition(); //checks if anyone won
+        if (Counter == 0){ //decides whos turn it is
             Counter += 1;
         }
         else if (Counter == 1) {
             Counter -= 1;
         }
-
+        //I'm too lazy to loop the game, if you want to play again, relauch the game
     }
     
 }
